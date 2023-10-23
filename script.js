8a5bde1ad1aa8ad0db8c004c62a01d42f7dbcb5f
ahmedelzaeem2 <147971142+ahmedelzaeem2@users.noreply.github.com>
2023-10-23 18:13:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+let userMessage = null; // Variable to store user's message
+let vid = document.querySelector("audio")
+
+
+const inputInitHeight = chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+    // Create a chat <li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", `${className}`);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; // return chat <li> element
+}
+
+
+function handleChat() {
+    userMessage = chatInput.value.trim().toLowerCase(); // Get user entered message and remove extra whitespace
+    if (!userMessage) return;
+
+    // Clear the input textarea and set its height to default
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+
+    // Append the user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+   
+}
+
+async function getBotAnswer() {
+
+
+    const respons = await fetch ("answer.json");
+    const data = await respons.json();
+    
+  data.forEach(answer => {
+
+  
+setTimeout(() => {
+    if (userMessage === answer.my_msg){
+        chatbox.appendChild(createChatLi(answer.bot_answer, "incoming"));
+        vid.play()
+  } 
+  
+
+ },700) ;
+
+ 
+     
+  });
+  
+}
+
+
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+   
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without Shift key and the window 
+    // width is greater than 800px, handle the chat
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+        getBotAnswer();
+    }
+});
+
+sendChatBtn.addEventListener("click", handleChat);
+sendChatBtn.addEventListener("click", getBotAnswer);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
